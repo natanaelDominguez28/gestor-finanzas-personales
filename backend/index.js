bc32d2fa9ab06f36816e94b1cdaf1807f3a6ec04
NatanaelDominguez <jorgedominguez349@gmail.com>
2026-04-12 22:06:19 -0300

    fix: corrección de error de tipeo en url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cors({origin: [
-    'https://gestor-finanzas-personales.gilt.vercel.app',
+    'https://gestor-finanzas-personales-gilt.vercel.app',
     'http://localhost:5173'],
     credentials: true
 }));
